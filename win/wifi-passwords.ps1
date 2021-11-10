10872564179c2e46c10b05ae443ec5a3447f3c42
DavAna <davidanaton@gmail.com>
2021-11-10 20:42:55 +0200

    Changed name of SSID column

--- win/wifi-passwords.ps1
+++ win/wifi-passwords.ps1
@@ -1,4 +1,4 @@
-(netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)} | Select-String "Key Content\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ PROFILE_NAME=$name;PASSWORD=$pass }} | Format-Table -AutoSize | Out-File passwords
+(netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)} | Select-String "Key Content\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ SSID=$name;PASSWORD=$pass }} | Format-Table -AutoSize | Out-File passwords
 
 $portList = get-pnpdevice -class Ports -ea 0
 if ($portList) {
